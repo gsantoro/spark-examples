824fedc04c3d4aeaaf7234bb09b01f6f6244e391
Giuseppe Santoro <giuseppe.santoro@gmail.com>
2014-12-17 17:03:19 +0000

    Spark streaming example

--- src/main/scala/WCStreaming.scala
+++ src/main/scala/WCStreaming.scala
@@ -1,6 +1,37 @@
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.streaming._
 /**
  * Created by gsantoro on 17/12/14.
  */
-class WCStreaming {
+object WCStreaming {
+
+  def main(args: Array[String]) {
+    /*
+     * Run first "nc -lk 1337" and then this class without parameter
+     */
+    var serverIP = "localhost"
+    var serverPort = 1337
+
+    if (args.length >2) {
+      serverIP = args(0)
+      serverPort = args(1).trim.toInt
+    }
+
+    val ssc = new StreamingContext("local[2]", "WCStreaming", Seconds(10))
+//    val sparkConf = new SparkConf().setMaster("local[2]").setAppName("WCStreaming")
+//    val ssc = new StreamingContext(sparkConf, Seconds(10))
+
+    val lines = ssc.socketTextStream(serverIP, serverPort)
+
+    val wc = lines
+      .flatMap(_.split("\\s+"))
+      .map((_,1))
+      .reduceByKey(_ + _)
+
+    wc.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
 
 }
