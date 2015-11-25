d51b29f44ac109e8db2728f03cfe18b0ace71999
Giuseppe Santoro <giuseppe.santoro@stratifiedmedica.com>
2015-11-25 19:54:59 +0000

    spark 1.5.2

--- src/main/scala/Teenagers.scala
+++ src/main/scala/Teenagers.scala
@@ -7,21 +7,21 @@ object Teenagers {
   case class Person(name: String, age: Int)
 
   def main(args: Array[String]) {
-    val peopleFile = "src/main/resources/people.txt"
-    val sparkConf = new SparkConf().setAppName("Teenagers")
-    val sc = new SparkContext(sparkConf)
-
-    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    import sqlContext.createSchemaRDD
-
-    val people = sc.textFile(peopleFile)
-      .map(_.split(","))
-      .map(p => Person(p(0), p(1).trim.toInt))
-
-    people.registerTempTable("people")
-
-    val teenagers = sqlContext.sql("SELECT name,age FROM people WHERE age >=20")
-
-    teenagers.map(t => "Name: " + t(0) + " age: " + t(1)).foreach(println)
+//    val peopleFile = "src/main/resources/people.txt"
+//    val sparkConf = new SparkConf().setAppName("Teenagers")
+//    val sc = new SparkContext(sparkConf)
+//
+//    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+//    import sqlContext.createSchemaRDD
+//
+//    val people = sc.textFile(peopleFile)
+//      .map(_.split(","))
+//      .map(p => Person(p(0), p(1).trim.toInt))
+//
+//    people.registerTempTable("people")
+//
+//    val teenagers = sqlContext.sql("SELECT name,age FROM people WHERE age >=20")
+//
+//    teenagers.map(t => "Name: " + t(0) + " age: " + t(1)).foreach(println)
   }
 }
